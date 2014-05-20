3ea660c89d8ab8a20dfaf0fd3976977f2e3304e5
Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
2014-05-20 10:58:27 +0100

    MongoDBConfigurationTest fixed

--- datastore-mongodb/src/test/java/org/opencb/datastore/mongodb/MongoDBConfigurationTest.java
+++ datastore-mongodb/src/test/java/org/opencb/datastore/mongodb/MongoDBConfigurationTest.java
@@ -2,19 +2,17 @@ package org.opencb.datastore.mongodb;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 /**
  * Created by imedina on 25/03/14.
+ * 
+ * @author Ignacio Medina Castelo <imedina@ebi.ac.uk>
+ * @author Cristina Yenyxe Gonzalez Garcia <cyenyxe@ebi.ac.uk>
  */
 public class MongoDBConfigurationTest {
-
-    @Before
-    public void setUp() throws Exception {
-        MongoDBConfiguration mongoDBConfiguration = new MongoDBConfiguration.Builder().build();
-        System.out.println(mongoDBConfiguration);
-    }
 
     @Test
     public void testInit() throws Exception {
@@ -27,5 +25,8 @@ public class MongoDBConfigurationTest {
         DB db = client.getDB("test");
 
         MongoDataStore mongoDataStore = new MongoDataStore(client, db, mongoDBConfiguration);
+        
+        assertNotNull(mongoDataStore);
+        assertEquals(db, mongoDataStore.getDb());
     }
 }
